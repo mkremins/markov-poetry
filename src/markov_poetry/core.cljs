c4755504818b1b4bae7bab2ff10f34bc19b5cc79
Max Kreminski <maxkreminski@gmail.com>
2014-02-11 20:11:06 -0800

    Core Markov chain construction fns

--- src/markov_poetry/core.cljs
+++ src/markov_poetry/core.cljs
@@ -1,5 +1,24 @@
 (ns markov-poetry.core)
 
-(enable-console-print!)
+(defn tokenize [text]
+  (->> (.split text "[^\\w'-]+") (map #(.toLowerCase %)) (into [])))
 
-(println "Hello world!")
+(defn markov-map [words]
+  (let [pairs (->> words (partition 2 1) (map vec))]
+    (reduce (fn [markov [word1 word2]]
+              (update-in markov [word1 word2] (fnil inc 0)))
+            {} pairs)))
+
+(defn weighted-choice [slices]
+  (->> slices
+    (map (fn [[value weight]] (repeat weight value)))
+    flatten
+    rand-nth))
+
+(defn markov-chain [start nexts]
+  (loop [chain start
+         last-word start]
+    (if (> 10 (* 100 (rand)) 0)
+      chain
+      (let [next-word (weighted-choice (get nexts last-word))]
+        (recur (str chain " " next-word) next-word)))))
